801c04b82082dda18efbb2cad6bd42d6b0b6462e
Dimas Ragil T <dynastymasra@gmail.com>
2019-08-21 13:10:32 +0700

    Add order respository

---
+++ infrastructure/repository/order.go
@@ -0,0 +1,50 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/dynastymasra/avalon/domain"
+	"github.com/dynastymasra/avalon/domain/repository"
+	"github.com/jinzhu/gorm"
+)
+
+const (
+	OrderTableName = "orders"
+)
+
+type OrderRepository struct {
+	db        *gorm.DB
+	TableName string
+}
+
+func NewOrderRepository(db *gorm.DB) *OrderRepository {
+	return &OrderRepository{
+		db:        db,
+		TableName: OrderTableName,
+	}
+}
+
+func (o OrderRepository) Save(ctx context.Context, order domain.Order) error {
+	return o.db.
+		Omit("created_at").
+		Table(o.TableName).
+		Save(&order).Error
+}
+
+func (o OrderRepository) Find(ctx context.Context, query domain.Order, filter *repository.Query) ([]*domain.Order, error) {
+	var result []*domain.Order
+
+	db := o.db.Table(o.TableName).Where(query)
+	db = translateQuery(db, filter)
+
+	err := db.Find(&result).Error
+
+	return result, err
+}
+
+func (o OrderRepository) Update(ctx context.Context, query domain.Order, update domain.Order) error {
+	return o.db.
+		Table(o.TableName).
+		Where(query).
+		Update(update).Error
+}
